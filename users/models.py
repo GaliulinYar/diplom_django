4456151ecec741090ee6bbd40cfd336db96633b4
Yaroslav Galiulin <yaroslav66@list.ru>
2023-11-27 11:58:18 +0500

    Создание модели Юзера

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    """Модель юзера"""
+    username = None
+    email = models.EmailField(max_length=30, unique=True, verbose_name='Почта')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
