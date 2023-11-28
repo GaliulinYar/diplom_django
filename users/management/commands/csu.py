4d89184ceda8d187f83e2417759a048f8015338d
Yaroslav Galiulin <yaroslav66@list.ru>
2023-11-28 18:11:12 +0500

    Скрипт создания супер юзера python manage.py csu

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,19 +1,26 @@
 from django.core.management import BaseCommand
-
-from user.models import User
+from django.db.utils import IntegrityError
+from users.models import User
 
 
 class Command(BaseCommand):
+    """Комманда для создания супер юзера -python manage.py csu- """
 
     def handle(self, *args, **options):
         """Добавляем админа"""
-        user = User.objects.create(
-            email='yaroslav66@list.ru',
-            first_name='Admin',
-            last_name='тут фамилия наверное',
-            is_staff=True,
-            is_superuser=True,
-        )
+        try:
+            user = User.objects.create(
+                email='yaroslav66@list.ru',
+                first_name='Admin',
+                last_name='тут фамилия наверное',
+                is_staff=True,
+                is_superuser=True,
+            )
 
-        user.set_password('qwerty')
-        user.save()
+            user.set_password('qwerty')
+            user.save()
+            print('Super User complete')
+
+        except IntegrityError as e:
+            print('Такой пользователь уже есть')
+
