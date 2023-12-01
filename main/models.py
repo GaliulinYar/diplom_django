206ac89b69d4e2b1918b4ee09381311a466a573c
Yaroslav Galiulin <yaroslav66@list.ru>
2023-12-01 13:38:16 +0500

    Небольшие исправления, написание документации

--- main/models.py
+++ main/models.py
@@ -1,20 +1,21 @@
-import os
-
 from django.db import models
 from django.conf import settings
-
-NULLABLE = {
-    'null': True,
-    'blank': True
-}
 
 
 # Create your models here.
 class Document(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Отправитель',
-                              **NULLABLE)  # Привязка к юзеру
+    # Класс отправляемого документя
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='postman_user')  # Привязка к юзеру
 
     file_load = models.FileField(upload_to='documents/', verbose_name='Файл')  # Сам файл
     check_file = models.BooleanField(default=False, verbose_name='Файл принят')  # Принятие файла, по у молчанию False
     checkout_file = models.BooleanField(default=False, verbose_name='Файл Отклонен')  # Отклонение  файла, по у молчанию False
 
+    # Строкое предстваления для разработчиков
+    def __str__(self):
+        return f'Документ {self.owner} Принятие - {self.check_file} Отклонение - {self.checkout_file}'
+
+    # Мета предстваление, например для админки
+    class Meta:
+        verbose_name = 'Документ'
+        verbose_name_plural = 'Документы'
--- users/models.py
+++ users/models.py
@@ -5,10 +5,10 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     """Модель юзера"""
-    username = None
-    email = models.EmailField(max_length=30, unique=True, verbose_name='Почта')
+    username = None  # для регистрации убираем имя, т.к. меняем его на почту
+    email = models.EmailField(max_length=30, unique=True, verbose_name='Почта')  # почта юзера
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = "email"  # при регистрации меняем поле имя на почту
     REQUIRED_FIELDS = []
 
     class Meta:
