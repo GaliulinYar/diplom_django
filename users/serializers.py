8674cb318fed7fe4763f7f38399c87f411d496cf
Yaroslav Galiulin <yaroslav66@list.ru>
2023-12-01 13:39:01 +0500

    Проверка сериализаторов юзера, пеп8, документация

--- users/serializers.py
+++ users/serializers.py
@@ -3,9 +3,30 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Сериализатор для юзера"""
+    """Сериализатор для создания юзера"""
 
     class Meta:
         # показываем все поля Юзера
         model = User
-        fields = ['email', 'first_name', 'id']
+        fields = '__all__'  # ['email', 'password', 'first_name', 'id']
+
+    # Шифруем пароль
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        # Устанавливаем зашифрованный пароль
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
+
+
+class UserDocSerializer(serializers.ModelSerializer):
+    """Сериализатор для отображения юзера """
+
+    class Meta:
+        # показываем нужные поля Юзера
+        model = User
+        fields = ['email', 'first_name']
