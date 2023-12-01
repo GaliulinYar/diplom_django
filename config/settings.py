66307eddc83d5dcc4a806f0b6712f2ae2315d2cd
Yaroslav Galiulin <yaroslav66@list.ru>
2023-12-01 14:14:09 +0500

    Добавление настроек документации redic и swagger

--- config/settings.py
+++ config/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'drf_yasg',
+
 
     'users',
     'main',
